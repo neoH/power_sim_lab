350e50303d39c150845ea79f967d2c28b3388937
neo <joachim_huang@icloud.com>
2018-11-13 12:35:36 +0800

    sync on macpro

--- design/tops/src/v/core/ifu/ifu.v
+++ design/tops/src/v/core/ifu/ifu.v
@@ -26,9 +26,10 @@ module ifu (
 	parameter ST_DEPTH = 16;
 
 	/* status definition */
-	// -- IDLE: reset mode, if jump valid, then change to JMP state, else change to NFTH state next cycle of IDLE.
-	// -- JMP
-	// -- NFTH: normal fetch mode, asserting the request.
+	// -- IDLE : reset mode, if jump valid, then change to JMP state, else change to NFTH state next cycle of IDLE.
+	// -- JMP  : jump mode, to judge and store jump actions.
+	// -- NFTH : normal fetch state, asserting the request.
+	// -- NHOLD: normal fetch hold state, then according to jmp signal and target valid signal to do the next.
 	// -- TODO
 	/* TODO */
 	/*********************/
@@ -43,22 +44,46 @@ module ifu (
 
 	// fetch request, these are self-defined request and response interface protocol
 	output o_FetchV; // fetch request valid signal
+	input  i_TrgtV;  // target valid signal, for each request, only this signal recevied high, then can next request be sended.
 	output [31:0] o_FetchT; // fetch request target
-	input i_TrgtV;   // target valid signal, for each request, only this signal recevied high, then can next request be sended.
 
 	/*************************/
 
 	/* internal reg & wire */
+	reg [1:0] r_stat,r_nstat;
 	/***********************/
 
 
 	always @(posedge i_Clk or negedge i_RstN) begin // {
 		if (!i_RstN) begin // {
+			r_nstat <= IDLE;
 		// }
 		end else begin // {
 			// using FSM
+			case (r_stat) // {
+				IDLE: begin // {
+				end // }
+				JMP: begin // {
+				end // }
+				NFTH: begin // {
+				end // }
+				NHOLD: begin // {
+				end // }
+				default: begin // {
+					r_nstat <= r_stat;
+				end // }
+			endcase // }
 		end // }
 	end // }
 
+	/* this block to change state according to next stat */
+	always @(posedge i_Clk or negedge i_RstN) begin // {
+		if (!i_RstN) begin // {
+			r_stat <= IDLE;
+		// }
+		end else begin // {
+			r_stat <= r_nstat;
+		end // }
+	end // }
 
 endmodule
